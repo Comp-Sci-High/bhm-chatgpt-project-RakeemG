a4a6c93c9ed3da547cde0bd7e9d1bda684c5eda7
RakeemG <rakeem.grace25@compscihigh.org>
2025-03-04 17:29:22 +0000

    Finished Project

--- index.js
+++ index.js
@@ -13,3 +13,13 @@ app.use((req, res, next) => {
     res.sendFile(__dirname + "/public/index.html")
   })
   
+  app.listen(3000, (req, res) => {})
+
+  function toggleInfo(id) {
+    const infoElement = document.getElementById(id);
+    const isHidden = infoElement.classList.contains('hidden');
+    document.querySelectorAll('.card-info').forEach(info => info.classList.add('hidden'));
+    
+    if (isHidden) {
+        infoElement.classList.remove('hidden');
+    }
